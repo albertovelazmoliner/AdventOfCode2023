82ba880a09a17a505a731cd2f75ae1834bf4d0e9
Alberto Vélaz Moliner <albertovelaz.dev@gmail.com>
2023-12-03 01:35:03 +0100

    Create Day2.kt

---
+++ src/Day2.kt
@@ -0,0 +1,78 @@
+import java.util.stream.IntStream.range
+
+fun main() {
+
+    val mapColors = mapOf(
+        "red" to 12,
+        "green" to 13,
+        "blue" to 14
+    )
+
+    fun part1(input: List<String>): Int {
+        var idSum = 0
+        for (text in input) {
+            val data = text.split(":")
+            val id = data[0].split(" ")[1].toInt()
+            val games = data[1].split("; ")
+            var isValid = true
+            for (game in games) {
+                val gameCubesSets = game.split(", ")
+                for (gameCubesSet in gameCubesSets) {
+                    val dataSet = gameCubesSet.trim().split(" ")
+                    val colorLimit = dataSet[1]
+                    if (dataSet[0].toInt() > mapColors[colorLimit]!!) {
+                        isValid = false
+                        break
+                    }
+                    if (!isValid) break
+                }
+            }
+            if (isValid) idSum += id
+        }
+        return idSum
+    }
+
+
+    fun part2(input: List<String>): Int {
+        var powerSum = 0
+        for (text in input) {
+            val games = text.split(":")[1].split("; ")
+            val powerMap = mutableMapOf<String, Int>()
+            var powerResult = 0
+            for (game in games) {
+                val gameCubesSets = game.split(", ")
+                for (gameCubesSet in gameCubesSets) {
+                    val dataSet = gameCubesSet.trim().split(" ")
+                    val colorLimit = dataSet[1]
+                    if (powerMap.containsKey(colorLimit)) {
+                        if (dataSet[0].toInt() > powerMap[colorLimit]!!) {
+                            powerMap[colorLimit] = dataSet[0].toInt()
+                        }
+                    } else {
+                        powerMap[colorLimit] = dataSet[0].toInt()
+                    }
+                }
+            }
+            powerMap.values.toList().forEach {
+                if (powerResult == 0) {
+                    powerResult = it
+                } else {
+                    powerResult *= it
+                }
+            }
+            powerSum += powerResult
+        }
+        return powerSum
+    }
+
+    //val testInput = readInput("Dummy")
+    val testInput = readInput("Test1Day2")
+
+    val result = part1(testInput)
+    print("result => ")
+    println(result)
+
+    val result2 = part2(testInput)
+    print("result part2 => ")
+    println(result2)
+}
