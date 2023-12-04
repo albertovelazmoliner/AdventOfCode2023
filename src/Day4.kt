41444576bba4eb90b625ee61fe94f4cdb3eda96d
Alberto Vélaz Moliner <albertovelaz.dev@gmail.com>
2023-12-04 09:21:34 +0100

    Update Day4.kt

--- src/Day4.kt
+++ src/Day4.kt
@@ -16,10 +16,39 @@ fun main() {
         }
         return totalWorth
     }
-    
+
+    fun part2(input: List<String>): Int {
+        val mapOfCards = mutableMapOf<String, Int>()
+        for (card in input.indices) {
+            if (mapOfCards.containsKey("$card")) mapOfCards["$card"] = mapOfCards["$card"]!! + 1
+            else mapOfCards["$card"] = 1
+            val winningNumbers = input[card].replace("   ", " ").replace("  ", " ")
+                .split(": ")[1].split(" | ")[0].split(" ")
+            val numbers = input[card].split(": ")[1].split(" | ")[1].replace("  ", " ")
+                .split(" ")
+            for (i in 0 until mapOfCards["$card"]!!) {
+                var hasWinningNumber = 0
+                numbers.forEach() {
+                    if (winningNumbers.contains(it)) {
+                        hasWinningNumber++
+                        if (mapOfCards.containsKey("${card + hasWinningNumber}")) {
+                            mapOfCards["${card + hasWinningNumber}"] = mapOfCards["${card + hasWinningNumber}"]!! + 1
+                        } else {
+                            mapOfCards["${card + hasWinningNumber}"] = 1
+                        }
+                    }
+                }
+            }
+        }
+        return mapOfCards.values.sum()
+    }
+
     val testInput = readInput("TestDay4")
 
     val result = part1(testInput)
     println("result => $result")
+
+    val result2 = part2(testInput)
+    println("result part2 => $result2")
 
 }
